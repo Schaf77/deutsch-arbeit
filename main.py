6e75aa684da8d38bc93224e8496747f0e3ee6429
Schaf77 <79912828+Schaf77@users.noreply.github.com>
2021-12-10 22:38:23 +0100

    Added second matrix (Arbeitsbedingungen)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ taetigkeiten_dev = ['pflegen, helfen, erziehen', 'Häuser, Brüken oder Fabriken
                 'kalkulieren und rechnen', 'Maschinen steuern und bedienen', 'bedienen, verkaufen, werben',
                 'montieren, reparieren', 'mit Kindern oder Jugendlichen zu tun haben']
 taetigkeiten = ['pflegen, helfen, erziehen', 'Häuser, Brüken oder Fabriken entwerfen oder bauen', 'kalkulieren und rechnen', 'Maschinen steuern und bedienen', 'bedienen, verkaufen, werben', 'montieren, reparieren', 'mit Kindern oder Jugendlichen zu tun haben', 'hauptsächlich entwerfen, gestalten, zeichnen', 'programmieren, EDV Systeme einrichten / verwalten', 'mit Material (Holz, Metall, Glas, Stein, ...) umgehen', 'zubereiten, kochen', 'zeichnen, gestalten, fotographieren', 'mit anderen Menschen in einer Fremdsprache reden', 'mit Literatur, Kultur, Medien umgehen', 'mich mit fremden Völkern, Kulturen... beschäftigen', 'messen, prüfen, untersuchen', 'eine medizinische Tätigkeit ausüben', 'in der Landwirtschaft oder im Gartenbau arbeiten', 'Texte verfassen', 'viel planen und organisieren', 'tanzen, musizieren, Theater spielen', 'menschliches Verhalten erforschen', 'anderen etwas beibringen können; unterrichten', 'mich mit politischen oder gesellschaftlichen Themen beschäftigen', 'sichern, schützen', 'mich mit philisophisch-theologischen Fragen beschäftigen', 'in Rechts- oder Steuerfragen beraten', 'Sport treiben können']
-
+arbeitsbedingungen = ['ein hohes Ansehen haben', 'eigenverantwortlich arbeiten', 'für mich alleine arbeiten', 'geregelte Arbeitszeit haben', 'viel unterwegs sein', 'mich selbstständig machen könnnen', 'gute Aufstiegsmöglichkeiten haben', 'mit anderen in der Gruppe zusammenarbeiten', 'im Ausland arbeiten können', 'im Freien arbeiten', 'viel Freizeit haben', 'einen sicheren Arbeitsplatz haben', 'Karriere machen', 'keine körperliche Belastung durch Lärm etc. haben', 'wenig Stress haben', 'in einem Büro arbeiten', 'im Sitzen arbeiten', 'überwiegend im Stehen arbeiten', 'andere Menschen führen', 'überwiegend mit dem Köpf arbeiten', 'überwiegend mit den Händen arbeiten', 'dass das Betriebsklima stimmt', 'ständig neues dazu lernen', 'mich engagieren und etwas voranbringen', 'dass meine Aufgaben klar strukturiert sind', 'einen kurzen Weg zu meiner Arbeitsstelle haben', 'viel selbst entscheiden können']
 question_list_1 = []
 question_list_2 = []
 already_asked = []
@@ -11,6 +11,7 @@ answers_dict = {}
 counter = 1
 question_list_1.extend(taetigkeiten)
 question_list_2 = question_list_1
+choose_prompt = 'In meinem künftigen Beruf möchte ich'
 
 # Code, der prüft, ob Fragen schonmal gestellt worden sind oder ob nach der selben Sache gefragt wird und ggf. abfragt und Ergebnisse in answers_dict einfügt
 for question_2 in question_list_1:
@@ -19,7 +20,7 @@ for question_2 in question_list_1:
             done = False
             while not done:
                 # print(f'question 1: {question_1}\nquestion 2: {question_2}')
-                antwort = input(f'In meinem künftigen Beruf möchte ich...\n{question_1}\noder\n{question_2}\n:')
+                antwort = input(f'{choose_prompt}...\n{question_1}\noder\n{question_2}\n:')
                 if antwort == '1':
                     answers_dict[f'{question_2} & {question_1}'] = int(question_list_1.index(question_1)) + 1
                     done = True
@@ -30,7 +31,6 @@ for question_2 in question_list_1:
                     already_asked.append(question_list_1.index(question_1) + question_list_1.index(question_2))
                 else:
                     print('Bitte mit 1 oder 2 antworten!')
-                # counter += 1
         else:
             break
 
